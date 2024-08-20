57ff7ca34b639eb8a5509503a22e47a5d00ee647
Jonathan Victor <jhonathanvictor142@gmail.com>
2024-08-20 16:20:02 -0300

    Projeto finalizado, integração com whatsapp

--- js/main.js
+++ js/main.js
@@ -1,15 +1,46 @@
+const formsCadastro = document.querySelector('#formsCadastro');
+
+if (formsCadastro) {
+    formsCadastro.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        let nome = document.getElementById('nome');
+        let email = document.getElementById('email');
+        let cpf = document.getElementById('cpf');
+        let dataNascimento = document.getElementById('dataNascimento');
+        let telefone = document.getElementById('telefone');
+
+        let mensagem = `*Cadastro de Curso*\n\n` +
+                    `*Nome:* ${nome.value}\n` +
+                    `*Email:* ${email.value}\n` +
+                    `*CPF:* ${cpf.value}\n` +
+                    `*Data de Nascimento:* ${dataNascimento.value}\n` +
+                    `*Telefone:* ${telefone.value}`;
+
+        let numeroWhatsApp = '5582996043922';
+
+        let urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
+
+        window.open(urlWhatsApp, '_blank');
+
+        nome.value = '';
+        email.value = '';
+        cpf.value = '';
+        dataNascimento.value = '';
+        telefone.value = '';
+    });
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     const itens = document.querySelectorAll('.categoria-lista-item');
     const cards = document.querySelectorAll('[data-item]');
 
     itens.forEach(item => {
         item.addEventListener('click', function () {
-            // Remove a classe 'ativo' de todos os itens do menu
             itens.forEach(itemMenu => {
                 itemMenu.classList.remove('ativo');
             });
 
-            // Adiciona a classe 'ativo' ao item do menu clicado
             this.classList.add('ativo');
 
             // Obtém a categoria do item do menu clicado
@@ -30,3 +61,28 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+const botoesEspecialista = document.querySelectorAll('.btn-falar-com-especialista');
+
+if (botoesEspecialista) {
+    botoesEspecialista.forEach(botao => {
+        botao.addEventListener('click', function(e) {
+            e.preventDefault();
+    
+            const card = this.closest('.card');
+    
+            const titulo = card.querySelector('.card-title').textContent.trim();
+            const descricao = card.querySelector('.card-text').textContent.trim();
+    
+            const mensagem = `*Falar com Especialista*\n\n` +
+                             `*Serviço:* ${titulo}\n`;
+    
+            const numeroWhatsApp = '5582996043922'; 
+    
+            const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
+    
+            window.open(urlWhatsApp, '_blank');
+        });
+    });
+};
+
